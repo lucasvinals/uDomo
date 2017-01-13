4dfb67e2c039b25189a3256f5ec3da3fdf5b3462
Lucas Viñals <lucas.vinals@gmail.com>
2017-01-13 13:47:09 -0300

    Create Configurations

--- app/REST/configurations.js
+++ app/REST/configurations.js
@@ -8,6 +8,52 @@ const Configurations = {
                 (!log.error(e) && callback(e, null)):
                 callback(null, c);
         });
+    },
+    CreateConfigurations: (config, callback) => {
+       
+        ConfigurationsSchema.findOne({Name: config.Name}, (e, c) => {
+            /**
+             * Error while saving the configuration. Log it and return the error
+             */
+            let errorSaving = (er) => {
+                log.error('> Something happened saving the configuration. ' + er);
+                callback('Algo paso guardando la configuración ' + config.Name, null);
+            };
+            /**
+             * Error while searching for the configuration. Log it and return the error
+             */
+            let errorSearching = () => {
+                log.error('> Something happened searching the configuration. ' + e);
+                callback('Algo paso buscando la configuracion \"' + 
+                            config.Name + '\"', null);
+            };
+            /**
+             * The configuration already exists in the database. Log and return a message
+             */
+            let configurationAlreadySaved = () => {
+                log.warning('> The configuration(\"' + config.Name + '\") ' +
+                                    'already exists in the database!');
+                callback('La configuración que se intenta guardar ' + 
+                            'ya existe en la base de datos.', null);
+            };
+            /**
+             * Returns the saved device
+             */
+            let savedConfiguration = (con) => {
+                log.success('> The configuration \"' + con.Name + '\" was saved.');
+                callback(null, con);
+            };
+            /** 
+             * Populate the model with the given device (request.body) and save it
+             */
+            let saveConfiguration = () => {
+                new ConfigurationsSchema(config).save((error, cSaved) => {
+                    error ? errorSaving(error) : savedConfiguration(cSaved);
+                });    
+            };
+
+            e ? errorSearching() : (c ? configurationAlreadySaved() : saveConfiguration());
+        });
     }
 };
 
@@ -16,6 +62,11 @@ module.exports = (app) => {
     .get('/api/Configurations', (request, response) => {
         Configurations.FindConfigurations((error, configs) => {
             response.json({"Configurations": configs, "Error": error});
+        });
+    })
+    .post('/Configuration', (request, response) => {
+        Configurations.CreateConfigurations(request.body, (e, c) => {
+            response.json({"Configuration": c, "Error": e});
         });
     });
 };
