68b384286fa174399ac1b79aa3d80bf0c7c00d79
Lucas Viñals <lucas.vinals@gmail.com>
2017-01-13 13:50:30 -0300

    Create configuration and remove dummy comments

--- client/js/controllers/users/users.js
+++ client/js/controllers/users/users.js
@@ -1,51 +1,76 @@
-
-Users.controller('userController', ['$scope', 'User',
-function($scope, User){
+Users.controller('userController', ['$scope', 'User', 'Common',
+function($scope, User, Common){
     'use strict';
 
-    /********************************* Read User ***************************************/
+    /**
+     * Init object for buttons
+     */
+    $scope.conf = {
+                    "Security"  : false,
+                    "Scenes"    : false,
+                    "Readings"  : false,
+                    "Devices"   : false,
+                    "Users"     : false
+    };
+
+    /**
+     * Read User
+     */
 	let getUsers = (function iifeGetUsers(){
         User.GetUsers((error, users) => {
           error ? log.error('Error obteniendo usuarios ->' + error) : $scope.users = users;
         });
         return iifeGetUsers;
     })();
-    /************************************************************************************/
      
-    /*************************** Observer pattern ***************************************/
+    /**
+     * Observer pattern
+     */
     User.Subscribe(getUsers);
-    /************************************************************************************/
         
-    /********************************* Remove User *************************************/
-    $scope.removeUser = (id) => {
-        User.DeleteUser(id);
-    };
-    /************************************************************************************/
+    /**
+     * Remove User
+     */
+    $scope.removeUser = (id) => { User.DeleteUser(id); };
 
-    /********************************* Modify User ***************************************/
-    $scope.modifyUser = (user) => {
-        User.ModifyUser(user);
-    };
-    /************************************************************************************/    
+    /**
+     * Modify User
+     */
+    $scope.modifyUser = (user) => { User.ModifyUser(user); };
 
-    /* Clean exit */
-    $scope.$on('$destroy', () => {
-        User.clearListeners();
-    });
+    /**
+     * Clean exit 
+     */
+    $scope.$on('$destroy', () => { User.clearListeners(); });
     
-    $scope.canRemove = () => {
-        return $rootScope.currentUser.Administrator === true ? true : false;
-    };
+    $scope.canRemove = () => $rootScope.currentUser.Administrator;
     
-    $scope.getPermissions = function(){
-        User.GetPermissions(function(permissions, errors){
+    /**
+     * Get Permissions
+     */
+    let getPermissions = (function iifePermissions() {
+        User.GetPermissions((error, permissions) => {
             $scope.permissions = permissions;
         });
-    };
+        return iifePermissions;
+    })();
 
-    $scope.getConfigurations = function(){
-        User.GetConfigurations(function(configurations, errors){
+    /**
+     * Get Configurations
+     */
+    let getConfigurations = (function iifeConfigurations() {
+        User.GetConfigurations((errors, configurations) => {
             $scope.configurations = configurations;
+        });
+        return iifeConfigurations;
+    })();
+
+    $scope.createConfiguration = (c) => {
+        c._id = Common.newID();
+        User.CreateConfiguration(c, (e, success) => {
+            e && 
+            log.error("Ocurrió un error creando la configuración") ||
+            log.success("Se creó la configuración.");
         });
     };
 }]);
