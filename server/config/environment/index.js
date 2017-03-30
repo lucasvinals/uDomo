981df73e709e9009a03d22727f3d6daee2110855
Lucas Viñals <lucas.vinals@gmail.com>
2017-03-29 22:29:43 -0300

    Define ROOTDIR, NODE_ENV and default port

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -1,8 +1,22 @@
 const { merge } = require('lodash');
+const { execSync } = require('child_process');
+/**
+ * Get the main app path
+ */
+process.ROOTDIR = execSync(
+  `${ process.platform === 'linux' ? 'realpath' : 'cd' } .`)
+  .toString()
+  .replace('\n', '')
+  .trim();
+/**
+ * Read the environment file or set NODE_ENV to 'development'
+ */
+process.env.NODE_ENV = process.env.NODE_ENV ||
+                       execSync(`cat ${ process.ROOTDIR }/environment`).toString().replace('\n', '') ||
+                       'development';
 
 module.exports = merge(
   {
-    ARG_CPU_NUMBER: 3,
-    DEFAULT_CLUSTER_PORT: 8080,
+    DEFAULT_CLUSTER_PORT: 12078,
   },
-  require(`./${ process.environment }`));
+  require(`./${ process.env.NODE_ENV }`));
