e6d57ad0887097bd8c2490f8e95ffc087a20334f
Lucas Viñals <lucas.vinals@gmail.com>
2017-04-03 21:19:52 -0300

    Use express.Router instead of plain route.

--- server/api/configuration/index.js
+++ server/api/configuration/index.js
@@ -1,7 +1,7 @@
 const Configuration = require('./configuration.controller');
 
-module.exports = (app) => {
-  app
-    .get('/api/Configurations', Configuration.Find)
-    .post('/Configuration', Configuration.Create);
-};
+module.exports = (router) =>
+  router
+    .get('/:id', Configuration.FindOne)
+    .get('/', Configuration.FindAll)
+    .post('/', Configuration.Create);
