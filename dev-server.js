64729cf179adb58f8d5c2c72294bf74a802be723
Lucas Viñals <lucas.vinals@gmail.com>
2017-04-26 02:54:21 -0300

    No proxy for now, only test client functioning

--- dev-server.js
+++ dev-server.js
@@ -1,20 +1,14 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const port = 8080;
 const config = require('./webpack.config');
+const port = 8080;
 
 new WebpackDevServer(
   webpack(config),
   {
     hot: true,
     filename: config.output.filename,
-    proxy: {
-      '*': 'http://localhost:12078',
-    },
-    contentBase: './udomo/views/',
     publicPath: config.output.publicPath,
-    stats: {
-      colors: true,
-    },
+    stats: { colors: true },
   }
 ).listen(port, 'localhost');
