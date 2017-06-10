befcb753372c7c72d4ad8a45852d57d2c181d4c9
Lucas Viñals <lucas.vinals@gmail.com>
2017-06-10 13:55:48 -0300

    :tada: webpack-dev-server with HMR working! :hammer: Bootstrap is now loaded from its local package. Index HTML file is minimized.

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,6 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const PRODUCTION = process.env.NODE_ENV === 'production';
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-const entry = PRODUCTION ?
-[
-  './client/js/app.js',
-] :
-/**
- * HMR (Hot Module Replacement) for development!
- */
-[
-  './client/js/app.js',
-  'webpack/hot/dev-server',
-  'webpack-dev-server/client?http://localhost:8080',
-];
-
 const plugins = PRODUCTION ?
 [
   new webpack.optimize.UglifyJsPlugin(
@@ -33,6 +20,20 @@ const plugins = PRODUCTION ?
   new HTMLWebpackPlugin(
     {
       template: './client/views/indexProduction.html',
+      filename: './views/index.html',
+      minify: {
+        minifyURLs: 'String',
+        removeComments: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeTagWhitespace: true,
+        useShortDoctype: true,
+        removeAttributeQuotes: true,
+        minifyJS: true,
+        minifyCSS: true,
+        caseSensitive: true,
+        collapseWhitespace: true,
+      },
     }
   ),
 ] :
@@ -47,7 +48,6 @@ const plugins = PRODUCTION ?
 plugins.push(
   new webpack.DefinePlugin(
     {
-      PRODUCTION: JSON.stringify(PRODUCTION),
       DEVELOPMENT: JSON.stringify(DEVELOPMENT),
     }
   )
@@ -55,7 +55,7 @@ plugins.push(
 
 module.exports = {
   devtool: 'source-map',
-  entry,
+  entry: './client/js/app.js',
   plugins,
   resolve: {},
   module: {
@@ -106,13 +106,52 @@ module.exports = {
            * For development use style-loader
            */
           [ 'style-loader', 'css-loader?localIdentName=[path][name]---[local]' ],
-        exclude: /node_modules/,
+        include: /node_modules\/bootstrap/,
+      },
+      /**
+       * Fonts
+       */
+      {
+        test: /\.(woff2?|woff|ttf|eot|svg)$/,
+        loader: 'url-loader?limit=10000&name=fonts/[name].[ext]',
       },
     ],
   },
   output: {
     path: path.join(__dirname, 'udomo'),
-    publicPath: PRODUCTION ? '/' : '/udomo/',
+    publicPath: PRODUCTION ? '/' : 'udomo/',
     filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js',
+  },
+  devServer: {
+    contentBase: './client/views',
+    /**
+     * This loads HMR on webpack.
+     */
+    hot: true,
+    /**
+     * Limit console messages to errors-only.
+     */
+    stats: 'errors-only',
+    /**
+     * Open a new browser tab when initialized
+     */
+    open: true,
+    /**
+     * Gzip content
+     */
+    compress: true,
+    /**
+     * Redirect all /api calls to uDomo backend
+     */
+    proxy: {
+      '/api': {
+        target: {
+          host: 'localhost',
+          port: 12078,
+          protocol: 'http:',
+        },
+        secure: false,
+      },
+    },
   },
 };
