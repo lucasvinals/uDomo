1fc883b050bd7a8042d336f0bab1a794cf7325b3
Lucas Viñals <lucas.vinals@gmail.com>
2017-06-11 02:13:03 -0300

    :boom: :art: ES6 Class, improve structure, resolve paths by ENV

--- client/js/app.routes.js
+++ client/js/app.routes.js
@@ -1,29 +1,171 @@
-import angular from 'angular';
-import { default as Storage } from './services/storage';
+import { config, inject } from 'ng-annotations';
 
-function RouteConfig($stateProvider, $httpProvider, $urlRouterProvider, $locationProvider) {
-  $httpProvider.interceptors.push([
-    '$q', '$location',
-    ($q, $location) => Object.assign(
-      {
-        'request': (config) => {
-          config.headers = config.headers || {};
-          const token = Storage.getToken();
-          if (token) {
-            config.headers.Authorization = `Bearer ${ token }`;
-          }
-          return config;
-        },
-        'responseError': (response) => {
-          if (response.status === 401 || response.status === 403) {  /* eslint no-magic-numbers:1 */
-            $location.path('/');
-          }
-          return $q.reject(response);
-        },
-      }
-    ),
-  ]
-);
+@config('Routes')
+@inject('$stateProvider', '$locationProvider', '$urlRouterProvider')
+export default class {
+  constructor($stateProvider, $locationProvider, $urlRouterProvider) {
+    this.stateProvider = $stateProvider;
+    // this.$httpProvider = $httpProvider;
+    this.urlRouterProvider = $urlRouterProvider;
+    this.locationProvider = $locationProvider;
+    // this.SetInterceptors();
+    this.States();
+    this.QuitHashes();
+  }
+  // SetInterceptors() {
+  //   this.$httpProvider.interceptors.push(
+  //     [
+  //       '$q', '$location',
+  //       ($q, $location) => Object.assign(
+  //         {
+  //           'request': (configuration) => {
+  //             configuration.headers = configuration.headers || {};
+  //             /**
+  //              * this.Storage.getToken();
+  //              */
+  //             const token = null;
+  //             if (token) {
+  //               config.headers.Authorization = `Bearer ${ token }`;
+  //             }
+  //             return config;
+  //           },
+  //           'responseError': (response) => {
+  //             if (response.status === Number('401') || response.status === Number('403')) {
+  //               $location.path('/');
+  //             }
+  //             return $q.reject(response);
+  //           },
+  //         }
+  //       ),
+  //     ]
+  //   );
+  // }
+
+  States() {
+    const viewPath = DEVELOPMENT ? '' : '/views';
+
+    this.stateProvider
+    /**
+    * Main Page - Contains all the information that persists.
+    * abstract:true mantains the content static
+    */
+      .state('uDomo', {
+        url: '/',
+        abstract: true,
+        // templateUrl: './views/index.html',
+        // controller: 'ControllerMain',
+      })
+      /**
+       * Homepage - Welcome and all the information to the user
+       */
+      .state('uDomo.home', {
+        url: '',
+        templateUrl: `.${ viewPath }/home/index.html`,
+        controller: 'ControllerHome',
+      })
+      /**
+       * User management
+       */
+      .state('uDomo.users', {
+        url: 'users',
+        templateUrl: `.${ viewPath }/user/index.html`,
+        controller: 'ControllerUser',
+      })
+      /**
+       * configuration scenarios for each group of users
+       */
+      .state('uDomo.users.configurations', {
+        url: 'user/configurations',
+        templateUrl: `.${ viewPath }/configuration/index.html`,
+        controller: 'ControllerUser',
+      })
+      /**
+       * Permissions
+       */
+      .state('uDomo.user.permissions', {
+        url: 'user/permissions',
+        templateUrl: `.${ viewPath }/permission/index.html`,
+        controller: 'ControllerUser',
+      })
+      /**
+       * Areas management
+       */
+      .state('uDomo.zones', {
+        url: 'zones',
+        templateUrl: `.${ viewPath }/zone/index.html`,
+        controller: 'ControllerZone',
+      })
+      /**
+       * Devices - Save, Program, view info, etc
+       */
+      .state('uDomo.devices', {
+        url: 'devices',
+        templateUrl: `.${ viewPath }/device/index.html`,
+        controller: 'ControllerDevice',
+      })
+      /**
+       * Scenes state and management
+       */
+      .state('uDomo.scenes', {
+        url: 'scenes',
+        templateUrl: `.${ viewPath }/scene/index.html`,
+        controller: 'ControllerScene',
+      })
+      /**
+       * Readings - Others
+       */
+      .state('uDomo.otherReadings', {
+        url: 'readings/others',
+        templateUrl: `.${ viewPath }/reading/others.html`,
+        controller: 'ControllerOtherSensors',
+      })
+      /**
+       * Readings - Server values
+       */
+      .state('uDomo.serverReadings', {
+        url: 'readings/server',
+        templateUrl: `.${ viewPath }/reading/server.html`,
+        controller: 'ControllerServerSensors',
+      })
+      /**
+       * Live cameras and other videos
+       */
+      .state('uDomo.videos', {
+        url: 'security/videos',
+        templateUrl: `.${ viewPath }/security/videos.html`,
+        controller: 'ControllerVideo',
+      })
+      .state('uDomo.perimeter', {
+        url: 'security/perimeter',
+        templateUrl: `.${ viewPath }/security/perimeter.html`,
+        controller: 'ControllerPerimeter',
+      })
+      .state('uDomo.warnings', {
+        url: 'security/warnings',
+        templateUrl: `.${ viewPath }/security/warnings.html`,
+        controller: 'ControllerPerimeter',
+      })
+      // /**
+      //  * 404 - Not Found page
+      //  */
+      .state('uDomo.notFound', {
+        url: '404',
+        templateUrl: `.${ viewPath }/notFound/index.html`,
+        controller: 'ControllerNotFound',
+      });
+    /**
+     * Otherwise redirect to the "404 - not found" page
+     */
+    // this.urlRouterProvider.otherwise('uDomo.notFound');
+    // this.urlRouterProvider.otherwise(($injector) => $injector.get('$state').go('uDomo.notFound'));
+  }
+
+  QuitHashes() {
+    /**
+     * Quits the hashes in the URL..
+     */
+    this.locationProvider.html5Mode({ enabled: true, requireBase: false });
+  }
 /*
 $http.get('/api/Routes', function(e, r){
 var routeProv = $routeProvider;
@@ -37,118 +179,4 @@ controller: ruta.controller
 });
 */
 // This setting should come of the DB.....
-
-  $stateProvider
-/**
-* Main Page - Contains all the information that persists.
-* abstract:true mantains the content static
-*/
-  .state('main', {
-    abstract: true,
-    url: '/',
-    templateUrl: '/views/index.html',
-    controller: 'ControllerMain',
-  })
-  /**
-   * Homepage - Welcome and all the information to the user
-   */
-  .state('home', {
-    url: '/',
-    templateUrl: '/views/home/home.html',
-    controller: 'homeController',
-  })
-  /**
-   * User management
-   */
-  .state('users', {
-    url: '/users',
-    templateUrl: '/views/users/users.html',
-    controller: 'userController',
-  })
-  /**
-   * configuration scenarios for each group of users
-   */
-  .state('users.configurations', {
-    url: 'configurations',
-    templateUrl: '/views/users/configurations.html',
-    controller: 'ConfigutarationsController',
-  })
-  /**
-   * Areas management
-   */
-  .state('areas', {
-    url: '/areas',
-    templateUrl: '/views/areas/areas.html',
-    controller: 'areaController',
-  })
-  /**
-   * Devices - Save, Program, view info, etc
-   */
-  .state('devices', {
-    url: '/devices',
-    templateUrl: '/views/devices/devices.html',
-    controller: 'deviceController',
-  })
-  /**
-   * Scenes state and management
-   */
-  .state('scenes', {
-    url: '/scenes',
-    templateUrl: '/views/scenes/scenes.html',
-    controller: 'sceneController',
-  })
-  /**
-   * Readings - Others
-   */
-  .state('otherReadings', {
-    url: '/readings/others',
-    templateUrl: '/views/readings/others.html',
-    controller: 'Others',
-  })
-  /**
-   * Readings - Server values
-   */
-  .state('serverReadings', {
-    url: '/readings/server',
-    templateUrl: '/views/readings/server.html',
-    controller: 'Server',
-  })
-  /**
-   * Live cameras and other videos
-   */
-  .state('videos', {
-    url: '/security/videos',
-    templateUrl: '/views/security/video.html',
-    controller: 'Video',
-  })
-  .state('permissions', {
-    url: '/users/permissions',
-    templateUrl: '/views/users/permissions.html',
-    controller: 'userController',
-  })
-  .state('configurations', {
-    url: '/users/configurations',
-    templateUrl: '/views/users/configurations.html',
-    controller: 'userController',
-  })
-  // /**
-  //  * 404 - Not Found page
-  //  */
-  .state('notFound', {
-    url: '/404',
-    templateUrl: '/views/home/notFound.html',
-    controller: 'notFoundController',
-  });
-  /**
-   * Otherwise redirect to the "404 - not found" page
-   */
-  // $urlRouterProvider.otherwise($stateProvider.go('notFound'));
-  /**
-   * Quits the hashes in the URL..
-   */
-  $locationProvider.html5Mode({ enabled: true, requireBase: false });
 }
-
-export default angular
-  .module('Route')
-  .config([ '$stateProvider', '$httpProvider', '$urlRouterProvider', '$locationProvider', RouteConfig ]);
