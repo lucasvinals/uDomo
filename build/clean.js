d7106de1390887599f5e84f72a612f53f9aa5857
Lucas Viñals <lucas.vinals@gmail.com>
2017-06-15 18:30:57 -0300

    :fire: Removed unused Gulp configuration files

--- build/clean.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const removeFiles = require('../server/tools/remove-files');
-
-const paths = [
-  `${ process.ROOTDIR }/udomo`,
-  `${ process.ROOTDIR }/*.log`,
-];
-
-gulp.task('clean', () => paths.map((path) => removeFiles(path)));
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-/**
- * Set environment and custom configurations.
- */
-require('./server/config/environment');
-/**
- * Load all tasks
- */
-require('require-dir')('./build', { recurse: true });
