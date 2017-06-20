7caceba36edf70f3cb77a44d6d7463f888bc30ae
Lucas Viñals <lucas.vinals@gmail.com>
2017-06-20 16:36:28 -0300

    :wrench: SSL certificates path and clusterPort in config

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -1,7 +1,13 @@
+const defaultPort = 12078;
+
 module.exports = {
   serverSensors: {
     python: '',
     javascript: '',
   },
-  clusterPort: process.env.CLUSTER_PORT || Number('12078'),
+  clusterPort: process.env.CLUSTER_PORT || defaultPort,
+  ssl: {
+    cert: `${ process.ROOTDIR }/server/ssl/server.crt`,
+    key: `${ process.ROOTDIR }/server/ssl/server.key`,
+  },
 };
