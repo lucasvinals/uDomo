6d6138a4118ed4a380ea3b76e07e4e05c8548e03
Lucas Viñals <lucas.vinals@gmail.com>
2017-07-09 03:36:04 -0300

    :beer: Some shit with socketio, must fix later

--- server/api/device/device.websocket.js
+++ server/api/device/device.websocket.js
@@ -1,49 +1,47 @@
 const { log } = process;
 const { merge, get, find, unset } = require('lodash');
 
-module.exports = (wsServer) => {
-  const wsDevice = wsServer('device');
-  wsDevice.Socket.on('message', console.log);
-  // io.on('connection', (socket) => {
-  //   socket.on('deviceRequest', (device) => {
-  //     /**
-  //      * Since the device is now online,
-  //      * set Online and lastMessage accordingly
-  //      */
-  //     merge(device, { Online: true, lastMessage: Date.now() });
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('deviceRequest', (device) => {
+      /**
+       * Since the device is now online,
+       * set Online and lastMessage accordingly
+       */
+      merge(device, { Online: true, lastMessage: Date.now() });
 
-  //     /**
-  //      * Merge the device into process.devices
-  //      */
-  //     merge(
-  //       find(process.devices, { _id: get(device, '_id', null) }),
-  //       device
-  //     );
-  //     io.sockets.emit('devices', process.devices);
-  //   });
-  //   socket.on('rChangePin', (request) => {
-  //     try {
-  //       const MAX = 1023;
-  //       switch (request.mode) {
-  //         case 'digital':
-  //           request.value = request.value === 0 ? MAX : 0;
-  //           break;
-  //         case 'analog':
-  //           if (request.value < 0 || request.value > MAX) {
-  //             throw new Error('The value has to be between 0 and 1023');
-  //           }
-  //           break;
-  //         default:
-  //           break;
-  //       }
-  //       socket.broadcast.emit('changePin', unset(request, 'mode'));
-  //     } catch (changePinError) {
-  //       log.error('Something happened trying to send the message to the device', changePinError);
-  //     }
-  //   });
+      /**
+       * Merge the device into process.devices
+       */
+      merge(
+        find(process.devices, { _id: get(device, '_id', null) }),
+        device
+      );
+      io.sockets.emit('devices', process.devices);
+    });
+    socket.on('rChangePin', (request) => {
+      try {
+        const MAX = 1023;
+        switch (request.mode) {
+          case 'digital':
+            request.value = request.value === 0 ? MAX : 0;
+            break;
+          case 'analog':
+            if (request.value < 0 || request.value > MAX) {
+              throw new Error('The value has to be between 0 and 1023');
+            }
+            break;
+          default:
+            break;
+        }
+        socket.broadcast.emit('changePin', unset(request, 'mode'));
+      } catch (changePinError) {
+        log.error('Something happened trying to send the message to the device', changePinError);
+      }
+    });
 
-  //   socket.on('disconnect', () => {
-  //     socket = null;
-  //   });
-  // });
+    socket.on('disconnect', () => {
+      socket = null;
+    });
+  });
 };
--- server/api/user/user.websocket.js
+++ server/api/user/user.websocket.js
@@ -1,15 +1,15 @@
 const User = require('./user.model');
 
-module.exports = (io) => {};
-  // io.on('connection', (socket) => {
-  //   /**
-  //    * Get all users
-  //    */
-  //   socket.on('Users/User/Read/Request', () =>
-  //     User
-  //       .find()
-  //       .exec()
-  //       .then((Users) => io.sockets.emit('Users/User/Read//Response', { Error: null, Users }))
-  //       .catch((findError) => io.sockets.emit('Users/User/Read/Response', { Error: findError, Users: [] }))
-  //   );
-  // });
+module.exports = (io) =>
+  io.on('connection', (socket) => {
+    /**
+     * Get all users
+     */
+    socket.on('Users/User/Read/Request', () =>
+      User
+        .find()
+        .exec()
+        .then((Users) => io.sockets.emit('Users/User/Read//Response', { Error: null, Users }))
+        .catch((findError) => io.sockets.emit('Users/User/Read/Response', { Error: findError, Users: [] }))
+    );
+  });
