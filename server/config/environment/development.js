cca3f7d46e842a6d26e77ae1cd6caf3f4bb3d956
Lucas Viñals <lucas.vinals@gmail.com>
2017-09-05 03:13:01 -0300

    :wrench: MongoDB connection for development environment :hammer:
    Add connection to MongoDB Atlas for development usage.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,9 +1,14 @@
+const USER = process.env.MONGODB_USER;
+const PASSWORD = process.env.MONGODB_PASSWORD;
+const REPLICA_NODES = [
+  'udomocluster-shard-00-00-isumt.mongodb.net:27017',
+  'udomocluster-shard-00-01-isumt.mongodb.net:27017',
+  'udomocluster-shard-00-02-isumt.mongodb.net:27017'
+];
+const REPLICA_SET = 'uDomoCluster-shard-0';
+
 module.exports = {
-  /**
-   * Change this to a different secret string!
-   */
-  entrophy: '3ud-!AMDepHhemCPh*n#',
   mongo: {
-    url: 'mongodb://127.0.0.1',
+    url: `mongodb://${USER}:${PASSWORD}@${REPLICA_NODES.join(',')}/uDomo?ssl=true&replicaSet=${REPLICA_SET}&authSource=admin`,
   },
 };
