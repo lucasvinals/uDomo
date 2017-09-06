129da2711cba7c599a2f888a4c0ac5fc6b926d0d
Lucas Viñals <lucas.vinals@gmail.com>
2017-09-06 01:24:48 -0300

    :book: Database connection string for development environment

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,14 +1,15 @@
 const USER = process.env.MONGODB_USER;
 const PASSWORD = process.env.MONGODB_PASSWORD;
+const ATLAS_MONGODB_PORT = 27017;
 const REPLICA_NODES = [
-  'udomocluster-shard-00-00-isumt.mongodb.net:27017',
-  'udomocluster-shard-00-01-isumt.mongodb.net:27017',
-  'udomocluster-shard-00-02-isumt.mongodb.net:27017'
+  `udomocluster-shard-00-00-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
+  `udomocluster-shard-00-01-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
+  `udomocluster-shard-00-02-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
 ];
 const REPLICA_SET = 'uDomoCluster-shard-0';
 
 module.exports = {
   mongo: {
-    url: `mongodb://${USER}:${PASSWORD}@${REPLICA_NODES.join(',')}/uDomo?ssl=true&replicaSet=${REPLICA_SET}&authSource=admin`,
+    url: `mongodb://${ USER }:${ PASSWORD }@${ REPLICA_NODES.join(',') }/uDomo?ssl=true&replicaSet=${ REPLICA_SET }&authSource=admin`,
   },
 };
