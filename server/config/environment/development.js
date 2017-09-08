ee60c0f950c3320f7a214475cedea163a01c7307
Lucas Viñals <lucas.vinals@gmail.com>
2017-09-08 04:12:32 -0300

    :wrench: Set MongoDB port with an environment variable.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,15 +1,23 @@
+/**
+ * Username and password of remote Atlas database
+ */
 const USER = process.env.MONGODB_USER;
 const PASSWORD = process.env.MONGODB_PASSWORD;
-const ATLAS_MONGODB_PORT = 27017;
+/**
+ * Replica nodes to connect with
+ */
 const REPLICA_NODES = [
-  `udomocluster-shard-00-00-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
-  `udomocluster-shard-00-01-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
-  `udomocluster-shard-00-02-isumt.mongodb.net:${ ATLAS_MONGODB_PORT }`,
+  `udomocluster-shard-00-00-isumt.mongodb.net:${ process.env.MONGODB_PORT }`,
+  `udomocluster-shard-00-01-isumt.mongodb.net:${ process.env.MONGODB_PORT }`,
+  `udomocluster-shard-00-02-isumt.mongodb.net:${ process.env.MONGODB_PORT }`,
 ];
 const REPLICA_SET = 'uDomoCluster-shard-0';
 
 module.exports = {
   mongo: {
-    url: `mongodb://${ USER }:${ PASSWORD }@${ REPLICA_NODES.join(',') }/uDomo?ssl=true&replicaSet=${ REPLICA_SET }&authSource=admin`,
+    url: `
+    mongodb://${ USER }:${ PASSWORD }@${ REPLICA_NODES.join(',') }
+    /uDomo?ssl=true&replicaSet=${ REPLICA_SET }&authSource=admin
+    `.replace(/[\n ]/g, ''),
   },
 };
--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -9,6 +9,12 @@ process.ROOTDIR = realpathSync('.');
  */
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 
+/**
+ * Set MongoDB port. 27017 by default.
+ */
+const DEFAULT_MONGODB_PORT = 27017;
+process.env.MONGODB_PORT = process.env.MONGODB_PORT || DEFAULT_MONGODB_PORT;
+
 module.exports = merge(
   require('./shared'),
   require(`./${ process.env.NODE_ENV }`)
