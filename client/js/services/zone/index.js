4fecafdf656963becb3e3ca66648835a3d565ca7
Lucas Viñals <lucas.vinals@gmail.com>
2017-09-20 02:38:43 -0300

    :muscle: Big refactor to work with resource layer. More structured methods

--- client/js/services/zone/index.js
+++ client/js/services/zone/index.js
@@ -1,112 +1,39 @@
 import { service, inject } from 'ng-annotations';
 
-@service('FactoryZone')
-@inject('$http', 'FactoryMessage', 'FactorySocket', 'FactoryObserver')
-export default class {
-  constructor(http, Message, Socket, Observer) {
-    this.http = http;
-    this.Observer = Observer;
-    this.Socket = Socket;
-    this.Message = Message;
-  }
-
-  ClearListeners() {
-    this.Observer.UnsubscribeAll();
-    this.Socket.Clear('Zones/Zone/Read/Response');
-    this.Socket.Emit('disconnect', {});
-  }
-
-  Subscribe(fn) {
-    return this.Observer.Subscribe(fn);
-  }
-
-  Unsubscribe(fn) {
-    return this.Observer.Unsubscribe(fn);
+@service()
+@inject('ResourceZone', 'FactoryCommon')
+export default class FactoryZone {
+  constructor(ResourceZone, Common) {
+    this.Zone = ResourceZone;
+    this.ProcessResponse = Common.ProcessResponse;
+    this.ThrowError = Common.ThrowError;
   }
 
   GetZones() {
-    return this.http
-      .get('/api/zone')
-      .then((zones) => {
-        const { Error: GetZonesError, Zones } = zones.data;
-        if (GetZonesError) {
-          this.Message.error(`Error: ${ JSON.stringify(GetZonesError) }`);
-          throw new Error('GetZonesError', JSON.stringify(GetZonesError));
-        }
-        return Zones;
-      })
-      .catch((httpError) => {
-        this.Message.error('Ocurrió un error con la consulta http');
-        window.log.error(httpError);
-      });
+    return this.Zone
+      .GetAll()
+      .then(this.ProcessResponse)
+      .catch(this.ThrowError);
   }
 
   CreateZone(zoneToCreate) {
-    return this.http.post('/api/zone', zoneToCreate)
-      .then((response) => {
-        const { Error: CreateZoneError, Zones } = response.data;
-        if (CreateZoneError) {
-          this.Message.error(`Error: ${ CreateZoneError }`);
-          window.log.error(CreateZoneError);
-          throw new Error(CreateZoneError);
-        }
-        this.Message.success(`El área ${ Zones.Name } fue creada.`);
-        this.Observer.Notify();
-        return Zones;
-      })
-      .catch((httpError) => {
-        this.Message.error('Ocurrió un error con la consulta http');
-        throw new Error(httpError);
-      });
+    return this.Zone
+      .Create(zoneToCreate)
+      .then(this.ProcessResponse)
+      .catch(this.ThrowError);
   }
 
   ModifyZone(zone) {
-    return this.http
-      .put('/api/zone', zone)
-      .then((modifyZoneResponse) => {
-        const { Error: ModifyZoneError, Zone } = modifyZoneResponse.data;
-        if (ModifyZoneError) {
-          this.Message.error(`Error: ${ JSON.stringify(ModifyZoneError) }`);
-          throw new Error('ModifyZoneError', JSON.stringify(ModifyZoneError));
-        }
-        return Zone;
-      })
-      .catch((httpError) => {
-        this.Message.error('Ocurrió un error con la consulta http');
-        throw new Error(httpError);
-      });
+    return this.Zone
+      .Modify(zone)
+      .then(this.ProcessResponse)
+      .catch(this.ThrowError);
   }
 
   DeleteZone(zoneId) {
-    return this.http
-      .delete(`/api/zone/${ zoneId }`)
-      .then(() => {
-        this.Message.success('La zona fue eliminada.');
-        this.Observer.Notify();
-        return zoneId;
-      })
-      .catch((httpError) => {
-        this.Message.error('Ocurrió un error con la consulta http');
-        throw new Error(httpError);
-      });
+    return this.Zone
+      .Delete(zoneId)
+      .then(this.ProcessResponse)
+      .catch(this.ThrowError);
   }
 }
-/* var self = this;
-  var requestZones = (function iifeReqZones(){
-      /* Emit the event so other users see the current Zones */
-      /* Socket.emit('Zones/Zone/Read/Request', {});
-      return iifeReqZones;
-  })();
-
-  Socket.on('Zones/Zone/Read/Response', function(data){
-      var error = data.Error;
-      var Zones = data.Zones;
-      if(!error){
-          return self.GetZones(null, Zones);
-      }else{
-          Message.error(`Ocurrió un error: ${error}`, 7);
-          log(error, 'error');
-          self.GetZones(error, null);
-      }
-  }); */
-
